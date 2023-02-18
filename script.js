cadc3879b975795b1ada1f139da6550c568a98b0
ryancole25 <cole.ryan.n@gmail.com>
2023-02-18 16:21:58 -0500

    Reorganized game logic; Checks for winner

--- script.js
+++ script.js
@@ -16,29 +16,9 @@ const createBoard = (() => {
     }
   }
 
-  const getBoard = () => console.log(board);
+  const getBoard = () => board;
 
-  const playMove = (shape) => {
-    const squares = document.querySelectorAll(".cell");
-    squares.forEach((square) =>
-      square.addEventListener("click", () => {
-        square.textContent = shape;
-        board[square.id - 1] = shape;
-        console.log(board);
-        togglePlayer(shape);
-      })
-    );
-  };
-
-  const togglePlayer = (shape) => {
-    if (shape == "X") {
-      playMove("O");
-    } else {
-      playMove("X");
-    }
-  };
-
-  return { getBoard, playMove };
+  return { getBoard };
 })();
 
 const Player = (shape) => {
@@ -47,11 +27,130 @@ const Player = (shape) => {
 };
 
 const gameController = (() => {
-  const humanPlayer = Player("X");
-  const computerPlayer = Player("O");
-  createBoard.playMove(humanPlayer.getShape());
-  return { humanPlayer, computerPlayer };
+  const player1 = Player("X");
+  const player2 = Player("O");
+  let player1turn = true;
+
+  const gameLogic = () => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) =>
+      cell.addEventListener("click", () => {
+        // Check if the cell is open
+        if (legalMove(cell.id - 1)) {
+          // Alternate turns
+          player1turn = decideTurn(player1turn);
+          if (!player1turn) {
+            shape = player1.getShape();
+          } else {
+            shape = player2.getShape();
+          }
+          // Draw the correct shape on the board
+          markBoard(cell, shape, cell.id - 1);
+          // Check to see if there is a winner
+          checkWinner(shape);
+        }
+      })
+    );
+  };
+
+  const legalMove = (index) => {
+    if (createBoard.getBoard()[index] == "") {
+      return true;
+    } else {
+      console.log("Not a legal move");
+      return false;
+    }
+  };
+
+  const markBoard = (cell, shape, index) => {
+    cell.textContent = shape;
+    createBoard.getBoard()[index] = shape;
+    console.log(createBoard.getBoard());
+  };
+
+  // Checks whose turn it is
+  const decideTurn = (player1turn) => {
+    return !player1turn;
+  };
+
+  const checkWinner = (shape) => {
+    // top row
+    if (
+      createBoard.getBoard()[0] == shape &&
+      createBoard.getBoard()[1] == shape &&
+      createBoard.getBoard()[2] == shape
+    ) {
+      console.log("winner");
+      return true;
+    }
+    // middle row
+    else if (
+      createBoard.getBoard()[3] == shape &&
+      createBoard.getBoard()[4] == shape &&
+      createBoard.getBoard()[5] == shape
+    ) {
+      console.log("winner");
+      return true;
+    }
+    // bottom row
+    else if (
+      createBoard.getBoard()[6] == shape &&
+      createBoard.getBoard()[7] == shape &&
+      createBoard.getBoard()[8] == shape
+    ) {
+      console.log("winner");
+      return true;
+    }
+
+    // first column
+    else if (
+      createBoard.getBoard()[0] == shape &&
+      createBoard.getBoard()[3] == shape &&
+      createBoard.getBoard()[6] == shape
+    ) {
+      console.log("winner");
+      return true;
+    }
+    // second column
+    else if (
+      createBoard.getBoard()[1] == shape &&
+      createBoard.getBoard()[4] == shape &&
+      createBoard.getBoard()[7] == shape
+    ) {
+      console.log("winner");
+      return true;
+    }
+    // third column
+    else if (
+      createBoard.getBoard()[2] == shape &&
+      createBoard.getBoard()[5] == shape &&
+      createBoard.getBoard()[8] == shape
+    ) {
+      console.log("winner");
+      return true;
+    }
+
+    // diagonal (top left - bottom right)
+    else if (
+      createBoard.getBoard()[0] == shape &&
+      createBoard.getBoard()[4] == shape &&
+      createBoard.getBoard()[8] == shape
+    ) {
+      console.log("winner");
+      return true;
+    }
+    // diagonal (top right - bottom left)
+    else if (
+      createBoard.getBoard()[2] == shape &&
+      createBoard.getBoard()[4] == shape &&
+      createBoard.getBoard()[6] == shape
+    ) {
+      console.log("winner");
+      return true;
+    }
+  };
+
+  return { player1, player2, gameLogic };
 })();
 
-// createBoard.getBoard();
-// createBoard.playMove("X");
+gameController.gameLogic();
